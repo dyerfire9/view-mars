b9216c1b33833e582861020576c35179fe1e08e2
dyerfire9 <canadianraza@yahoo.com>
2022-12-26 01:51:07 -0500

    created form inputs

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,24 +1,57 @@
 import React from "react"
+import './style.css'
 
+export default function Dashboard(){    
+    let [data, setData] = React.useState()
+    let [formData, setFormData] = React.useState({
+        sol: '',
+        cameratype: '',
+    })
 
-export default function Dashboard(){
-    // let [data, setData] = React.useState()
+    function handleChange(event) {
+        setFormData(prevFormData =>{
+            const {name, value, type, checked} = event.target
 
-    // React.useEffect(() => {
-    //     // Now this fetch data call will run only once since the dependencies array is empty
-    //     fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=555&camera=fhaz&api_key=XrhklQkPEfhtwohJSVqusnTh1VSATt2AkS4fKcPn")
-    //     .then(res => res.json())
-    //     .then(data => {setData(data)})
-    //     .catch(err => {
-    //       console.log(err)
-    //     })
-    // }, [])  
+            return{
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+        console.log(formData)
+    }
+
+    React.useEffect(() => {
+        // Now this fetch data call will run only once since the dependencies array is empty
+        fetch("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=555&camera=NAVCAM&api_key=XrhklQkPEfhtwohJSVqusnTh1VSATt2AkS4fKcPn")
+        .then(res => res.json())
+        .then(data => {setData(data)})
+        .catch(err => {
+          console.log(err)
+        })
+    }, [])  
 
     return (
         <div>
-            {/* photos[0].img_src */}
+            <div className="form">
+                <h2 className="form-header">Input Parameters</h2>
+               <input 
+                    type="text"
+                    className="form-input"
+                    value={formData.sol}
+                    name="sol"
+                    onChange={handleChange}  />
+
+                <input 
+                    type="text"
+                    className="form-input"
+                    value={formData.cameratype}
+                    name="cameratype"
+                    onChange={handleChange} />
+            </div>
+
+            {/* {photos[0].img_src} */}
             {/* {console.log(data.photos[1])} */}
-            {/* <img src={data.photos[1].img_src} alt="" /> */}
+           {/* {<img src={data.photos[2].img_src} alt="" />} */}
         </div>
     )
 }
