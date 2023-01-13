290b7d9b2e07b39a8d68b607c948e05badec9adc
dyerfire9 <canadianraza@yahoo.com>
2023-01-13 00:17:21 -0500

    updated Contact Component for mobiles only

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import { BsLinkedin, BsGithub, BsMailbox2, BsFillCreditCard2FrontFill } from "re
 
 export default function Footer(){
     const style1 = {color: "#c1440e", fontSize: "1.8rem", verticalAlign: "text-bottom", marginRight: '2px'}
+    const style2 = {color: "#c1440e", fontSize: "1.5rem", verticalAlign: "text-bottom", marginRight: '2px'}
 
     return (
         <div className="footer-container">
@@ -15,16 +16,17 @@ export default function Footer(){
                         for quenching your curiosity. Feel free to reach out to me for any feedback or questions regarding this app.
                     </h2>
                 </div>
+                
                 <div className="footer-contact">
                 <h2 className="footer-title">Contact</h2>
                     <div className="footer-contact-content"> 
                         <h2 className="footer-contact-name">Abdul Mannan</h2>
                         <p className="footer-contact-bio">A Computer Science & Statistics Student at University of Toronto</p>
                         <div className="footer-contact-links">
-                            <a href="https://github.com/dyerfire9/" target="_blank" className="footer-links"><BsMailbox2 style={style1}/>Email</a>
-                            <a href="https://github.com/dyerfire9/" target="_blank" className="footer-links"><BsGithub style={style1}/>Github</a>
-                            <a href="https://linkedin.com/in/abdulmannancomp/" target="_blank" className="footer-links"><BsLinkedin style={style1}/>LinkedIn</a>
-                            <a href="https://dyerfire9.github.io/portfolio-site/" target="_blank" className="footer-links"><BsFillCreditCard2FrontFill style={style1}/>Website</a>
+                            <a href="https://github.com/dyerfire9/" target="_blank" className="footer-links"><BsMailbox2 style={window.width ? style1: style2}/>Email</a>
+                            <a href="https://github.com/dyerfire9/" target="_blank" className="footer-links"><BsGithub style={window.width ? style1: style2}/>Github</a>
+                            <a href="https://linkedin.com/in/abdulmannancomp/" target="_blank" className="footer-links"><BsLinkedin style={window.width ? style1: style2}/>LinkedIn</a>
+                            <a href="https://dyerfire9.github.io/portfolio-site/" target="_blank" className="footer-links"><BsFillCreditCard2FrontFill style={window.width ? style1: style2}/>Website</a>
                         </div>
                     </div>
                 </div>
