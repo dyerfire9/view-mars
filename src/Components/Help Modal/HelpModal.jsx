8bd9401e9672a8eeccb9f7bb0db5a3f08b91f275
dyerfire9 <canadianraza@yahoo.com>
2023-02-12 22:15:53 -0500

    updated help module

--- src/Components/Help Modal/HelpModal.jsx
+++ src/Components/Help Modal/HelpModal.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-
+import "./style.css"
 export default function HelpModal(props){
     
     return(
         <div className="help-modal">
             <div className="help-modal-container">
                 <h2>Help Module</h2>
+                <div className="help-header">
+
+                </div>
+                <div className="help-content">
+                    <ul>
+
+                    </ul>
+                </div>
             </div>
         </div>
     )
